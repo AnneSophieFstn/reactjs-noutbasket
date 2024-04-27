4d8bef1e78152445f41322c26b395fc2e69b60cc
Anne-Sophie <annesophie.faustino@gmail.com>
2024-04-27 17:29:04 +0400

    change url server

--- src/_helpers/axiosConfig.js
+++ src/_helpers/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const urlAPI = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://nodejs-api-noutbasket.onrender.com",
 });
 
 export default urlAPI;
